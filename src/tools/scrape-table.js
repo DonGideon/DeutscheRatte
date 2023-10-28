fe7fb2e97500e634ecb09965ca277b7a0f28095b
dongideon <gur.gideon@gmail.com>
2023-10-28 13:19:50 +0200

    tool: table scraper

---
+++ src/tools/scrape-table.js
@@ -0,0 +1,32 @@
+const cheerio = require('cheerio')
+
+async function scrapeTable() {
+  const response = await fetch('https://en.evdealmanca.com/most-common-german-nouns-top-100/')
+
+  if (!response.ok) {
+    throw new Error(`Failed to scrape table: ${response.statusText}`)
+  }
+
+  const html = await response.text()
+
+  const $ = cheerio.load(html)
+
+  const table = $('table')
+
+  const rows = table.find('tr')
+
+  const articlesAndNouns = {}
+
+  for (const row of rows) {
+    const secondColumn = $(row).find('td:nth-child(3)')
+
+    const cleanArticleAndNoun = secondColumn.text().replace(/[^a-zA-Z\s]/g, '').trim() //remove all emojis and new lines 
+
+    if (!!cleanArticleAndNoun) { // 'article noun'
+        const articleAndNounArr = cleanArticleAndNoun.split(' ') // [article, noun]
+        articlesAndNouns[articleAndNounArr[0]] = articleAndNounArr[1]
+    }
+  }
+
+  return articlesAndNouns
+}

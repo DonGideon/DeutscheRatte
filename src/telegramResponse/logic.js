5672d3a37803ee3286c1257e48d504b6645d9704
dongideon <gur.gideon@gmail.com>
2023-10-30 17:37:09 +0100

    telegramResponse: improve logic of set-get questions

--- src/telegramResponse/logic.js
+++ src/telegramResponse/logic.js
@@ -1,46 +1,68 @@
-const {getRandomValue} = require('../tools/db/dbApi')
+const {initQuestions, getCurrentQuestion, checkCurrentQuestion, getPreviosAnswer, getSummary} = require('./bodyCreator')
+
+const questionsData = {
+    numOfQuestions: 10,
+    currentQuestionIndex: 0,
+    rightOrWrong: [],
+    questions: []
+}
+
+const keyboard = {
+    keyboard: [[
+        {
+            text: 'Der',
+        }
+    ],
+    [
+        {
+            text: 'Das',
+        }
+    ],
+    [
+        {
+            text: 'Die',
+        }
+    ]],
+    one_time_keyboard: true
+}
 
 const responseLogic = (requestBody) => {
-    if (!questionsData.questions[0]) getQuestions()
     const requestData = JSON.parse(requestBody)
+    const userText = requestData.message.text
+    
     const responseBody = {
         chat_id: requestData.message.chat.id,
-        reply_markup: {
-            keyboard: [[
-                {
-                    text: 'Der',
-                }
-            ],
-            [
-                {
-                    text: 'Das',
-                }
-            ],
-            [
-                {
-                    text: 'Die',
-                }
-            ]]
-        },
-        text: getCurrentQuestion()
+        text: ''
+    }
+
+    if (['Der', 'Das', 'Die'].includes(userText) && // answer
+        questionsData.currentQuestionIndex != 0 && // not first round
+        questionsData.currentQuestionIndex <= questionsData.numOfQuestions) { // not after last round
+        responseBody.text = checkCurrentQuestion(questionsData, userText)
+        responseBody.text += " "
+        responseBody.text += getPreviosAnswer(questionsData)
+        responseBody.text += "\n\n"
+        if (questionsData.currentQuestionIndex < questionsData.numOfQuestions) { // move to next round
+            responseBody.text += `nächstes Wort: *${getCurrentQuestion(questionsData)}*`
+            responseBody.reply_markup = keyboard
+        } else { //finish the game
+            responseBody.text += "\n\n"
+            responseBody.text += getSummary(questionsData)
+            responseBody.text += "\n\n"
+            responseBody.text += "start over? press 's'"
+        }
+    } else if (['start', 's'].includes(userText.toLowerCase())) { // new game
+        initQuestions(questionsData)
+        responseBody.text = `erstes Wort: *${getCurrentQuestion(questionsData)}*`
+        responseBody.reply_markup = keyboard
+    } else if (userText == '🐀') { // ❤️
+        responseBody.text = "❤️"
+    } else { // sugest to start the game
+        responseBody.text = "Press 's' to start 🐀"
     }
 
     return responseBody
 }
 
-
-const questionsData = {
-    numOfQuestions: 10,
-    currentQuestionIndex: 0,
-    rightAndWrong: [],
-    questions: []
-}
-
-const getQuestions = () => {
-    for(let i=0 ; i<questionsData.numOfQuestions; i++) {
-        questionsData.questions.push(getRandomValue())
-        questionsData['rightAndWrong'].push(false)
-    }
-}
 
 module.exports = {responseLogic}

30760750f933fe3358475063eb9ac9f7af3bd399
dongideon <gur.gideon@gmail.com>
2023-10-30 17:37:59 +0100

    telegramResponse: response for every kind of request

--- src/telegramResponse/bodyCreator.js
+++ src/telegramResponse/bodyCreator.js
@@ -1,21 +1,46 @@
+const {getRandomValue} = require('../tools/db/dbApi')
 
-const getCurrentQuestion = () => {
-    // [article, noun]
-    return questionsData.questions[questionsData['currentQuestionIndex']][1]
+const initQuestions = (questionsData) => {
+    questionsData['questions'] = []
+    questionsData['rightOrWrong'] = []
+    questionsData['currentQuestionIndex'] = 0
+
+    for(let i=0 ; i<questionsData.numOfQuestions; i++) {
+        questionsData.questions.push(getRandomValue())
+        questionsData.rightOrWrong.push(false)
+    }
 }
 
-const checkCurrentQuestion = (answer) => {
-    const isRightAnswer = (questionsData.rightAndWrong[questionsData['currentQuestionIndex']][0] === answer)
-    if (isRightAnswer) {
-        questionsData.rightAndWrong[questionsData['currentQuestionIndex']] = true
-    }
+const getCurrentQuestion = (questionsData) => {
+    // [article, noun] => noun
+    const currentQuestion = questionsData.questions[questionsData['currentQuestionIndex']][1]
     questionsData['currentQuestionIndex']++
-    return isRightAnswer
+    return currentQuestion
 }
 
-const getSummary = () => {
-    for(let i=0 ; i<questionsData['numOfQuestions']; i++) {
+const getPreviosAnswer = (questionsData) => {
+    // [article, noun] => 'article noun'
+    return questionsData.questions[questionsData['currentQuestionIndex']-1].join(' ')
+}
 
+const checkCurrentQuestion = (questionsData, answer) => {
+    const isRightAnswer = (questionsData.questions[questionsData['currentQuestionIndex']-1][0].toLowerCase() === answer.toLowerCase())
+    if (isRightAnswer) {
+        questionsData.rightOrWrong[questionsData['currentQuestionIndex']-1] = true
     }
-
+    return isRightAnswer ? "✅😌✅" : "❌😐❌"
 }
+
+const getSummary = (questionsData) => {
+    let summary = ''
+    let countRights = 0
+    for(let i=0 ; i<questionsData.numOfQuestions; i++) {
+        summary += `${questionsData.questions[i].join(' ')} ${questionsData.rightOrWrong[i] ? '✅' : '❌'}\n`
+        if (questionsData.rightOrWrong[i]) countRights++
+    }
+    const relativeEmoji = countRights/questionsData.numOfQuestions > 0.5 ? '🎉' : '🫠'
+    summary += `${countRights}/${questionsData.numOfQuestions} ${relativeEmoji}`
+    return summary
+}
+
+module.exports = {initQuestions, getCurrentQuestion, checkCurrentQuestion, getSummary, getPreviosAnswer}

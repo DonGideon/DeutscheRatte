2e149f06028653de16eee6337dfd2282b14735d9
dongideon <gur.gideon@gmail.com>
2023-10-30 22:22:28 +0100

    test: telegramResponse/logic

---
+++ src/telegramResponse/logic.test.js
@@ -0,0 +1,24 @@
+
+const {responseLogic} = require('./logic')
+  
+describe('responseLogic Function', () => {
+    const mockedRequestBody = JSON.stringify({
+        message: {
+        text: 'textValueToReplace',
+        chat: { id: 123 },
+        },
+    })
+
+    it('should handle "🐀" input', () => {
+        const requestBody = mockedRequestBody.replace('textValueToReplace', '🐀')
+        const result = responseLogic(requestBody)
+        expect(result.text).toMatch('❤️')
+    })
+
+    it('should suggest starting the game', () => {
+        const requestBody = mockedRequestBody.replace('textValueToReplace', 'RandomText')
+        const result = responseLogic(requestBody)
+        expect(result.text).toMatch('Press \'s\' to start 🐀')
+    })
+})
+  
